04e7e4c1a0d6e97fd412f42b631ba9de45a5340f
meow <again@nira.lan>
2024-09-01 19:12:31 +0545

    prolly:last for i386

--- compile.sh
+++ compile.sh
@@ -1,4 +1,5 @@
-i386-elf-gcc -std=gnu99 -ffreestanding -g -c start.s -o start.o
+i386-elf-as start.s -o start.o
+i386-elf-gcc -c kernel.c -o kernel.o -std=gnu99 -ffreestanding -O2 -Wall -Wextra
 i386-elf-gcc -std=gnu99 -ffreestanding -g -c kernel.c -o kernel.o
-i386-elf-gcc -ffreestanding -nostdlib -g -T linker.ld start.o kernel.o -o mykernel.elf -lgcc
+i386-elf-gcc -T linker.ld -o myos.bin -ffreestanding -O2 -nostdlib start.o kernel.o -lgcc
 
